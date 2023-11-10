1670a15e2f7bd17689a4f15efcd2f16e648ee332
m-nayab <110473732+m-nayab@users.noreply.github.com>
2023-11-10 22:34:22 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBL4vX9gVeWenOMiFi9wP_43JfTqOH4VMU",
+    authDomain: "e-commerce-13450.firebaseapp.com",
+    projectId: "e-commerce-13450",
+    storageBucket: "e-commerce-13450.appspot.com",
+    messagingSenderId: "948905256139",
+    appId: "1:948905256139:web:ece322ada5c27a85eea74e"
+  };
+  
